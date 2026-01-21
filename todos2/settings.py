5135febca56fdec5973670c03898a04115edd6b7
Riyaa <riyabhawsar5@gmail.com>
2026-01-21 13:12:44 +0530

    solve sitting problem

--- todos2/settings.py
+++ todos2/settings.py
@@ -11,19 +11,21 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os, environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+env=environ.Env(DEBUG=(bool,False))
+environ.Env.read_env(os.path.join(BASE_DIR,'.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q8qib=3ajvl&g^hl-t!$-$n*^@&=^u)y8za^7&y!kdp!cn30kl'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =env('DEBUG') 
 
 ALLOWED_HOSTS = []
 
